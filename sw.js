02171ec0ef0981273cc6d47c367933baeff0f156
ElCampeones <campeones.ae@gmail.com>
2025-07-13 23:39:10 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Service Worker للتخزين المؤقت
+const CACHE_NAME = 'campeones-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/images/logo.png',
+  '/images/about.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
